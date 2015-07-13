49c2d9cb61b8a37668a071d7d0a3bc5790d0c968
Surya Kiran <suryak@motorola.com>
2015-07-12 21:17:34 -0700

    Updating second page activity to interact with database

--- app/src/main/java/com/surya/intentserviceapp/activity/SecondPageActivity.java
+++ app/src/main/java/com/surya/intentserviceapp/activity/SecondPageActivity.java
@@ -11,6 +11,11 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.surya.intentserviceapp.R;
+import com.surya.intentserviceapp.db.dao.DAOFactory;
+import com.surya.intentserviceapp.db.dao.IFdnDAO;
+import com.surya.intentserviceapp.dto.FdnDTO;
+
+import java.util.Random;
 
 
 public class SecondPageActivity extends Activity {
@@ -51,19 +56,39 @@ public class SecondPageActivity extends Activity {
     public void onPostNotificationClick(View view) {
         Log.i(TAG, "Inside onPostNotificationClick method");
 
+        Random r = new Random();
+        FdnDTO fdn = new FdnDTO();
+        int notificationNumber = r.nextInt(20);
+        fdn.setFdnId("FDN_" + notificationNumber);
+        fdn.setContentTitle("Title " + notificationNumber);
+        fdn.setContentText("Text for " + notificationNumber);
+
+        fdn = saveFdn(fdn);
+        postNotificationToTray(fdn);
+
+        TextView tv = (TextView) findViewById(R.id.notifId);
+        tv.setText(fdn.getFdnId() + " -> id : " + fdn.getId());
+
+        Log.i(TAG, "Exiting onPostNotificationClick method");
+    }
+
+    private void postNotificationToTray(FdnDTO fdn) {
+        Log.i(TAG, "Inside postNotificationToTray method");
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.notification_template_icon_bg)
-                .setContentTitle("Blah Blah Title")
-                .setContentText("Blah Blah Blah - This is my text");
+                .setContentTitle(fdn.getContentTitle())
+                .setContentText(fdn.getContentText())
+                .setPriority(fdn.getAndroidPriority());
 
         Intent onClickIntent = new Intent(this, SecondPageActivity.class);
         onClickIntent.setAction(ACTION_CLICK);
-        onClickIntent.putExtra(ACTION_CLICK, "Surya");
+        onClickIntent.putExtra(ACTION_CLICK, fdn.getContentTitle() + " clicked");
         PendingIntent onClickPendingIntent = PendingIntent.getActivity(getApplicationContext(), 12345, onClickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         Intent onClearIntent = new Intent(this, SecondPageActivity.class);
         onClearIntent.setAction(ACTION_DISMISS);
-        onClearIntent.putExtra(ACTION_DISMISS, "Kiran");
+        onClearIntent.putExtra(ACTION_DISMISS, fdn.getContentTitle() + " dismissed");
         PendingIntent onClearPendingIntent = PendingIntent.getActivity(getApplicationContext(), 54321, onClearIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         builder.setContentIntent(onClickPendingIntent);
@@ -73,6 +98,24 @@ public class SecondPageActivity extends Activity {
         NotificationManager notifMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notifMgr.notify(notificationId, builder.build());
 
-        Log.i(TAG, "Exiting onPostNotificationClick method");
+        Log.i(TAG, "Exiting postNotificationToTray method");
+    }
+
+    private FdnDTO saveFdn(FdnDTO fdn) {
+        Log.i(TAG, "Inside saveFdn method");
+
+        IFdnDAO fdnDao = DAOFactory.getInstance().getFdnDAO(getApplicationContext());
+        fdn = fdnDao.save(fdn);
+
+        Log.i(TAG, "Exiting saveFdn method");
+        return fdn;
+    }
+
+    public void onListAllNotificationsClick(View view) {
+        Log.i(TAG, "Inside onListAllNotificationsClick method");
+
+
+
+        Log.i(TAG, "Exiting onListAllNotificationsClick method");
     }
 }
